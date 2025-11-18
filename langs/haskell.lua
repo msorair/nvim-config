b5e5522708ead96a70909f4ddd34a617656a132c
lingshin <yokaringx@outlook.com>
2025-11-18 20:01:54 +0800

    fix(langs.haskell): add eval keymap

--- langs/haskell.lua
+++ langs/haskell.lua
@@ -18,9 +18,9 @@ return {
       keys = {
         {
           "<localleader>e",
-          "<cmd>HlsEvalAll<cr>",
+          function() require("haskell-tools").lsp.buf_eval_all() end,
           ft = "haskell",
-          desc = "Evaluate All",
+          desc = "Evaluate",
         },
         {
           "<localleader>h",
@@ -30,12 +30,6 @@ return {
         },
         {
           "<localleader>r",
-          function() require("haskell-tools").repl.toggle() end,
-          ft = "haskell",
-          desc = "REPL (Package)",
-        },
-        {
-          "<localleader>R",
           function() require("haskell-tools").repl.toggle(vim.api.nvim_buf_get_name(0)) end,
           ft = "haskell",
           desc = "REPL (Buffer)",
