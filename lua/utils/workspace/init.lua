5dc3fdba41389fe4eda462490f04edca6e8a4dee
lingshin <yokaringx@outlook.com>
2025-12-08 14:12:01 +0800

    feat(utils.workspace): add opts to each handers, and support workspace specific language options

--- lua/utils/workspace/init.lua
+++ lua/utils/workspace/init.lua
@@ -1,19 +1,26 @@
 ---@alias config.workspace.entry {name:string, type:"regular"|"directory"}
----@alias config.workspace.dir_handler fun(dir: string)
----@alias config.workspace.file_handler fun(value: any)
+---@alias config.workspace.dir_handler fun(dir: string, opts?: config.workspace.hander.Opts)
+---@alias config.workspace.file_handler fun(value: any, opts?: config.workspace.hander.Opts)
 
----@class config.workspace.HandlerOpts
+---@class config.workspace.hander.Opts
+---@field root string
+---@field dir string
+---@field path string
+
+---@class config.workspace.handler.Config
 ---@field directory table<string, config.workspace.dir_handler>
 ---@field file table<string, config.workspace.dir_handler|boolean>
 
 ---@class config.workspace.Opts
----@field handlers config.workspace.HandlerOpts
+---@field handlers config.workspace.handler.Config
+---@field dir string
 
 local uv = vim.uv
 local fs = require "utils.fs"
 
 ---@type config.workspace.Opts
 local config = {
+  dir = ".nvim",
   handlers = {
     directory = {
       langs = require("utils.workspace.langs").handle_dir,
@@ -25,17 +32,20 @@ local config = {
   },
 }
 
-local function handle_each(dir, basename, filetype)
-  local path = string.format("%s/%s", dir, basename)
+local function handle_each(basename, filetype, opts)
+  local dir = opts.dir
+  local root = opts.root
+  local path = string.format("%s/%s/%s", root, dir, basename)
+  opts.path = path
   if filetype == "directory" then
     local handler = config.handlers["directory"][basename]
-    if handler then handler(dir .. "/" .. basename) end
+    if handler then handler(path, opts) end
   elseif filetype == "file" and basename:sub(-4, -1) == ".lua" then
     local name = basename:sub(1, -5)
     local handler = config.handlers["file"][name]
     if handler then
       if type(handler) == "function" then
-        handler(dofile(path))
+        handler(dofile(path), opts)
       else
         dofile(path)
       end
@@ -43,12 +53,22 @@ local function handle_each(dir, basename, filetype)
   end
 end
 
-local function detect_workspace(dir)
-  dir = dir .. "/.nvim"
-  uv.fs_stat(dir, function(err, stat)
+local function detect_workspace(root)
+  local dot_nvim = string.format("%s/%s", root, config.dir)
+  uv.fs_stat(dot_nvim, function(err, stat)
     if err then return end
     if stat and stat.type == "directory" then
-      fs.ls(dir, vim.schedule_wrap(function(file, type) handle_each(dir, file, type) end))
+      fs.ls(
+        dot_nvim,
+        vim.schedule_wrap(
+          function(file, type)
+            handle_each(file, type, {
+              root = root,
+              dir = config.dir,
+            })
+          end
+        )
+      )
     end
   end)
 end
--- lua/utils/workspace/langs.lua
+++ lua/utils/workspace/langs.lua
@@ -10,9 +10,38 @@ local plugins_loaded = function()
   })
 end
 
+---@param dir string
+---@param file string
+---@return boolean
+local function isFileIn(dir, file) return file:find(dir, 1, true) == 1 end
+
+local function config_options(lang, opts)
+  if not lang.options then return end
+  local callback = function(event)
+    local file = vim.api.nvim_buf_get_name(event and event.buf or 0)
+    if not isFileIn(opts.root, file) then return end
+    for opt, value in pairs(lang.options or {}) do
+      vim.opt_local[opt] = value
+    end
+  end
+  if vim.bo.filetype == lang.name then callback() end
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = lang.name,
+    desc = string.format("Set options for filetype %s in workspace %s", lang.name, opts.root),
+    callback = callback,
+  })
+end
+
+local function config_langs(langs, opts)
+  for _, lang in pairs(langs.get) do
+    config_options(lang, opts)
+  end
+  langs:config { "lsp", "mason", "treesitter", "formatter" }
+end
+
 return {
   ---@type config.workspace.dir_handler
-  handle_dir = function(dir)
+  handle_dir = function(dir, opts)
     local loaded = plugins_loaded()
     local langs = loaded and Langs.new() or require "config.language"
     fs.ls(dir, function(file, type)
@@ -22,15 +51,15 @@ return {
         if not config[1] then config[1] = name end
         langs:solve(config)
       end
-    end, loaded and vim.schedule_wrap(function() langs:config() end) or nil)
+    end, loaded and vim.schedule_wrap(function() config_langs(langs, opts) end) or nil)
   end,
 
   ---@type config.workspace.file_handler
-  handle_file = function(config)
+  handle_file = function(config, opts)
     local loaded = plugins_loaded()
     local langs = loaded and Langs.new() or require "config.language"
     if not config[1] then return end
     langs:solve(config)
-    if loaded then langs:config() end
+    if loaded then config_langs(langs, opts) end
   end,
 }
