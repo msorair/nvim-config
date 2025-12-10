849bb0a7fd563617702ab4fc02615006d19760fd
lingshin <yokaringx@outlook.com>
2025-12-10 12:46:02 +0800

    format(plugins.overseer)

--- lua/utils/plugin/overseer.lua
+++ lua/utils/plugin/overseer.lua
@@ -5,7 +5,11 @@ local create_cmd = vim.api.nvim_create_user_command
 overseer.add_template_hook(nil, function(task_defn, util)
   if util.has_component(task_defn, "on_output_quickfix") then
     util.remove_component(task_defn, { "on_output_quickfix" })
-    util.add_component(task_defn, { "lingshin.on_output_trouble", open_on_exit = "failure", close = true })
+    util.add_component(task_defn, {
+      "lingshin.on_output_trouble",
+      open_on_exit = "failure",
+      close = true,
+    })
   end
 end)
 
@@ -30,15 +34,21 @@ end, {})
 create_cmd("Make", function(params)
   local cmd, num_subs = vim.o.makeprg:gsub("%$%*", params.args)
   if num_subs == 0 then cmd = cmd .. " " .. params.args end
-  local task = overseer.new_task {
-    cmd = vim.fn.expandcmd(cmd),
-    components = {
-      { "lingshin.on_output_trouble", open_on_match = not params.bang, close = true, set_diagnostics = true },
-      "on_result_diagnostics",
-      "default",
-    },
-  }
-  task:start()
+  overseer
+    .new_task({
+      cmd = vim.fn.expandcmd(cmd),
+      components = {
+        "on_result_diagnostics",
+        "default",
+        {
+          "lingshin.on_output_trouble",
+          open_on_match = not params.bang,
+          close = true,
+          errorformat = vim.o.errorformat,
+        },
+      },
+    })
+    :start()
 end, {
   desc = "Run your makeprg as an Overseer task",
   nargs = "*",
@@ -49,19 +59,20 @@ create_cmd("Grep", function(params)
   -- Insert args at the '$*' in the grepprg
   local cmd, num_subs = vim.o.grepprg:gsub("%$%*", params.args)
   if num_subs == 0 then cmd = cmd .. " " .. params.args end
-  local task = overseer.new_task {
-    cmd = vim.fn.expandcmd(cmd),
-    components = {
-      {
-        "lingshin.on_output_trouble",
-        errorformat = vim.o.grepformat,
-        open_on_match = not params.bang,
-        items_only = true,
+  overseer
+    .new_task({
+      cmd = vim.fn.expandcmd(cmd),
+      components = {
+        {
+          "lingshin.on_output_trouble",
+          errorformat = vim.o.grepformat,
+          open_on_match = not params.bang,
+          items_only = true,
+        },
+        -- We don't care to keep this around as long as most tasks
+        { "on_complete_dispose", timeout = 30 },
+        "default",
       },
-      -- We don't care to keep this around as long as most tasks
-      { "on_complete_dispose", timeout = 30 },
-      "default",
-    },
-  }
-  task:start()
+    })
+    :start()
 end, { nargs = "*", bang = true, complete = "file" })
