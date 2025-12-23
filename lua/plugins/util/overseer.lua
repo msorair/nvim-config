98ec3d8e03f7a8486d2e15851b2e6dd4615b71d1
lingshin <yokaringx@outlook.com>
2025-12-23 16:07:19 +0800

    feat(plugins.oil): add action and keymap to open quickfix automatically, simpler watch, btw

--- lua/plugins/util/overseer.lua
+++ lua/plugins/util/overseer.lua
@@ -20,20 +20,40 @@ return {
   },
   opts = {
     dap = false,
+    actions = {
+      trouble = {
+        desc = "open trouble on detecting error messages",
+        condition = function(task) return not task:has_component "lingshin.on_output_trouble" end,
+        run = function(task)
+          task:set_component { "lingshin.on_output_trouble", open_on_match = true, errorformat = vim.o.errorformat }
+        end,
+      },
+      watch = {
+        desc = "restart the task when you save a file",
+        condition = function(task) return not task:has_component "restart_on_save" end,
+        run = function(task) task:set_component "restart_on_save" end,
+      },
+    },
     task_list = {
       direction = "right",
       keymaps = {
         ["<C-s>"] = false,
+        ["<C-j>"] = false,
+        ["<C-k>"] = false,
+        ["<C-t>"] = false,
         ["<C-v>"] = false,
         ["<C-e>"] = false,
+        ["p"] = false,
         ["o"] = false,
         ["g?"] = false,
         ["R"] = { "keymap.run_action", opts = { action = "watch" }, desc = "Auto task" },
         ["r"] = { "keymap.run_action", opts = { action = "restart" }, desc = "Restart task" },
         ["i"] = { "keymap.run_action", opts = { action = "edit" }, desc = "Edit task" },
+        ["<C-q>"] = { "keymap.run_action", opts = { action = "trouble" }, desc = "Quickfix" },
         ["|"] = { "keymap.open", opts = { dir = "vsplit" }, desc = "Open task output in vsplit" },
         ["-"] = { "keymap.open", opts = { dir = "split" }, desc = "Open task output in split" },
         ["<Tab>"] = { "keymap.open", opts = { dir = "tab" }, desc = "Open task output in tab" },
+        ["K"] = { "keymap.toggle_preview", desc = "Preview" },
       },
     },
   },
