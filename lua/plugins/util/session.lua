a755abb185895225db6b4ee81456b2e5a3dd23cd
lingshin <yokaringx@outlook.com>
2025-12-23 13:06:30 +0800

    feat(plugins.persistence): add keymap to save session manually

--- lua/plugins/util/session.lua
+++ lua/plugins/util/session.lua
@@ -4,7 +4,8 @@ return {
   event = "BufReadPre", -- this will only start session saving when an actual file was opened
   -- load the session for the current directory
   keys = {
-    { "<leader>qs", function() require("persistence").select() end, desc = "Select" },
+    { "<leader>qp", function() require("persistence").select() end, desc = "Pick" },
+    { "<leader>qs", function() require("persistence").save() end, desc = "Save" },
     { "<leader>ql", function() require("persistence").load { last = true } end, desc = "Last" },
   },
   opts = {
