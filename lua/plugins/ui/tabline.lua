b9bc8717f58d72ab4a14a4d99a991036953a0f70
lingshin <yokaringx@outlook.com>
2025-12-25 17:23:13 +0800

    feat(plugins.tabline): tweak appearance

--- lua/plugins/ui/tabline.lua
+++ lua/plugins/ui/tabline.lua
@@ -3,17 +3,18 @@ return {
   event = "VeryLazy",
   keys = {
   },
-  ---
   config = function()
     local get = require("utils.highlight.fn").getter
     local set = require("utils.highlight.fn").setter
     local current = set.Normal { bold = true, italic = true }
     local tabline = set.Comment { bg = "#151924" }
+    local stress = get["@comment.warning"]
     local tabicon = get.Special
     local space = " "
     local theme = {
       icon = tabicon,
       fill = tabline,
+      stress = stress,
       head = { fg = get.Tabline.bg, bg = get.Special.fg },
       current_tab = current,
       tab = tabline,
@@ -31,32 +32,39 @@ return {
         return {
           line.tabs().foreach(function(tab)
             local hl = tab.is_current() and theme.current_tab or theme.tab
-            local jump_key = tab.jump_key()
+            local icon = tab.in_jump_mode() and { tab.jump_key(), hl = tab.is_current() and theme.icon or theme.stress }
+              or tab.is_current() and { "󰻂", hl = theme.icon }
+              or number[tab.number()]
+              or "󰆣"
             return {
-              jump_key == "" and space or jump_key,
-              tab.is_current() and { "󰻂", hl = theme.icon } or number[tab.number()] or "󰆣",
+              space,
+              icon,
+              space,
               tab.name(),
               space,
               hl = hl,
-              margin = space,
             }
           end),
           line.spacer(),
           line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-            local icon, hl = Snacks.util.icon(win.buf_name())
+            local buf = win.buf()
+            local name = buf.name()
+            local icon, hl = Snacks.util.icon(name)
             local opts = { buffer = win.buf().id }
             return {
               space,
               { icon, hl = hl },
-              win.buf_name(),
-              hl = win.is_current() and theme.current_win or theme.win,
+              space,
+              name,
               grapple.exists(opts) and {
+                space,
                 "󰛢",
                 grapple.name_or_index(opts),
                 space,
-                hl = get["@comment.warning"],
+                hl = theme.stress,
               },
-              margin = space,
+              space,
+              hl = win.is_current() and theme.current_win or theme.win,
             }
           end),
           { "  ", hl = theme.tail },
