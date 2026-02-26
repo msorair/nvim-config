9b47e706a148a33ac16ec057b7da6c0566b133bd
lingshin <yokaringx@outlook.com>
2026-02-26 22:51:02 +0800

    fix(utils.mason): use pkg.spec.bin to decide whether to install

--- lua/utils/plugin/mason.lua
+++ lua/utils/plugin/mason.lua
@@ -4,11 +4,11 @@ return {
   end,
   install = function(pkg_name)
     local registry = require "mason-registry"
-    if vim.fn.executable(pkg_name) then
-      vim.notify(pkg_name .. " found locally, skipping")
-    elseif not registry.is_installed(pkg_name) and registry.has_package(pkg_name) then
-      vim.notify("installing " .. pkg_name .. "..")
+    if not registry.is_installed(pkg_name) and registry.has_package(pkg_name) then
       local pkg = registry.get_package(pkg_name)
+      local all_installed = not vim.iter(pkg.spec.bin):any(function(k, _) return not vim.fn.executable(k) end)
+      if all_installed then return end
+      vim.notify("installing " .. pkg_name .. "..")
       pkg:install():once("closed", function()
         vim.schedule(function() vim.notify(pkg_name .. (pkg:is_installed() and " is " or " not ") .. "installed") end)
       end)
