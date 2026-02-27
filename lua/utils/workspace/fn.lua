8835a29aca64f6544dc067672ec4996335d4a01c
lingshin <yokaringx@outlook.com>
2026-02-27 19:34:18 +0800

    format(utils.workspace.fn)

--- lua/utils/workspace/fn.lua
+++ lua/utils/workspace/fn.lua
@@ -1,6 +1,6 @@
----@param dir string
----@param file string
----@return boolean
 return {
+  ---@param dir string
+  ---@param file string
+  ---@return boolean
   is_file_in = function(dir, file) return file:find(dir, 1, true) == 1 end,
 }
