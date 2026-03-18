1bc428a7f671996e9a883f02803ec038159e2208
lingshin <yokaringx@outlook.com>
2026-03-18 14:32:57 +0800

    fix(utils.mason): fix the wrong usage of executable

--- lua/utils/plugin/mason.lua
+++ lua/utils/plugin/mason.lua
@@ -6,7 +6,7 @@ return {
     local registry = require "mason-registry"
     if not registry.is_installed(pkg_name) and registry.has_package(pkg_name) then
       local pkg = registry.get_package(pkg_name)
-      local all_installed = not vim.iter(pkg.spec.bin):any(function(k, _) return not vim.fn.executable(k) end)
+      local all_installed = vim.iter(pairs(pkg.spec.bin)):all(function(k, _) return vim.fn.executable(k) == 1 end)
       if all_installed then return end
       vim.notify("installing " .. pkg_name .. "..")
       pkg:install():once("closed", function()
