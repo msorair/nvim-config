debce0af1200902e5ef6d1fa389bd92338319348
msorair <sorair42@gmail.com>
2026-03-20 22:49:09 +0800

    build: update dependencies

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,9 +1,11 @@
-if not vim.g.neovide then return end
+if not vim.g.neovide then
+	return
+end
 
 vim.g.neovide_padding_top = 10
 vim.g.neovide_padding_right = 0
 
-vim.g.neovide_opacity = 0.95
+-- vim.g.neovide_opacity = 0.95
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_input_ime = true
 vim.g.neovide_underline_stroke_scale = 1.0
@@ -15,14 +17,16 @@ vim.g.neovide_cursor_smooth_blink = true
 
 vim.g.neovide_input_ime = false
 vim.api.nvim_create_autocmd({
-  "InsertEnter",
-  "InsertLeave",
-  "TermEnter",
-  "TermLeave",
-  "CmdlineEnter",
-  "CmdlineLeave",
+	"InsertEnter",
+	"InsertLeave",
+	"TermEnter",
+	"TermLeave",
+	"CmdlineEnter",
+	"CmdlineLeave",
 }, {
-  group = vim.api.nvim_create_augroup("ime_input", { clear = true }),
-  pattern = "*",
-  callback = function(args) vim.g.neovide_input_ime = not not args.event:match "Enter$" end,
+	group = vim.api.nvim_create_augroup("ime_input", { clear = true }),
+	pattern = "*",
+	callback = function(args)
+		vim.g.neovide_input_ime = not not args.event:match("Enter$")
+	end,
 })
